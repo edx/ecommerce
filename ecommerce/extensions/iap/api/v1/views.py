cf2592429d21fb87705f803de157cf112d6b1fae
jawad khan <jkhan@2u.com>
2025-01-30 16:20:09 +0500

    feat: Enable mobile payments for new upcoming courses
    feat: Enable mobile payments for new upcoming courses

--- ecommerce/extensions/iap/api/v1/views.py
+++ ecommerce/extensions/iap/api/v1/views.py
@@ -4,6 +4,7 @@ import time
 
 import app_store_notifications_v2_validator as asn2
 import httplib2
+import waffle
 from django.conf import settings
 from django.db import transaction
 from django.utils.decorators import method_decorator
@@ -84,6 +85,7 @@ from ecommerce.extensions.iap.api.v1.constants import (
 from ecommerce.extensions.iap.api.v1.exceptions import RefundCompletionException
 from ecommerce.extensions.iap.api.v1.serializers import MobileOrderSerializer
 from ecommerce.extensions.iap.api.v1.utils import create_ios_product, products_in_basket_already_purchased
+from ecommerce.extensions.iap.constants import CREATE_APPSTORE_PRODUCTS_FOR_INAPP
 from ecommerce.extensions.iap.models import IAPProcessorConfiguration
 from ecommerce.extensions.iap.processors.android_iap import AndroidIAP
 from ecommerce.extensions.iap.processors.ios_iap import IOSIAP
@@ -487,18 +489,19 @@ class MobileSkusCreationView(APIView):
             course.publish_to_lms()
             created_skus[course_run_key] = [mobile_products[0].partner_sku, mobile_products[1].partner_sku]
 
-            # create ios product on appstore
-            partner_short_code = request.site.siteconfiguration.partner.short_code
-            configuration = settings.PAYMENT_PROCESSOR_CONFIG[partner_short_code.lower()][IOSIAP.NAME.lower()]
-            ios_product = list((filter(lambda sku: 'ios' in sku.partner_sku, mobile_products)))[0]
-            course_data = {
-                'price': ios_product.price_excl_tax,
-                'name': course.name,
-                'key': course_run_key
-            }
-            error_msg = create_ios_product(course_data, ios_product, configuration)
-            if error_msg:
-                failed_ios_products.append(error_msg)
+            if waffle.switch_is_active(CREATE_APPSTORE_PRODUCTS_FOR_INAPP):
+                # create ios product on appstore
+                partner_short_code = request.site.siteconfiguration.partner.short_code
+                configuration = settings.PAYMENT_PROCESSOR_CONFIG[partner_short_code.lower()][IOSIAP.NAME.lower()]
+                ios_product = list((filter(lambda sku: 'ios' in sku.partner_sku, mobile_products)))[0]
+                course_data = {
+                    'price': ios_product.price_excl_tax,
+                    'name': course.name,
+                    'key': course_run_key
+                }
+                error_msg = create_ios_product(course_data, ios_product, configuration)
+                if error_msg:
+                    failed_ios_products.append(error_msg)
 
         result = {
             'new_mobile_skus': created_skus,
