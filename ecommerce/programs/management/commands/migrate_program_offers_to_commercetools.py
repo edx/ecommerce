cc0bee52d8d48241f3fca4ffc5601200348f43d6
Muhammad Noyan  Aziz <noyan.aziz@000969400357.2tor.net>
2025-02-20 20:05:17 +0500

    fix: quality checks

--- ecommerce/programs/management/commands/migrate_program_offers_to_commercetools.py
+++ ecommerce/programs/management/commands/migrate_program_offers_to_commercetools.py
@@ -26,19 +26,20 @@ CLIENT_ID = "your_client_id"
 CLIENT_SECRET = "your_client_secret"
 SCOPES = "manage_project:your_project_key"
 
-class ProxyClassDiscountType(Enum):
-    PERCENTAGE = "ecommerce.programs.benefits.PercentageDiscountBenefitWithoutRange"
-    ABSOLUTE = "ecommerce.programs.benefits.AbsoluteDiscountBenefitWithoutRange"
-
 CT_ABSOLUTE_DISCOUNT_TYPE = 'absolute'
 CT_PERCENTAGE_DISCOUNT_TYPE = 'relative'
 
 TEN_PERCENT_DISCOUNT_PERMYRIAD = 1000
 
 CT_CART_DISCOUNT_TYPE_MAP = {
-    Benefit.FIXED:CT_ABSOLUTE_DISCOUNT_TYPE,
+    Benefit.FIXED: CT_ABSOLUTE_DISCOUNT_TYPE,
     Benefit.PERCENTAGE: CT_PERCENTAGE_DISCOUNT_TYPE
 }
+
+
+class ProxyClassDiscountType(Enum):
+    PERCENTAGE = "ecommerce.programs.benefits.PercentageDiscountBenefitWithoutRange"
+    ABSOLUTE = "ecommerce.programs.benefits.AbsoluteDiscountBenefitWithoutRange"
 
 
 def _query_existing_discount(client: CommercetoolsAPIClient, discount_type: str, discount_value: int):
@@ -48,6 +49,7 @@ def _query_existing_discount(client: CommercetoolsAPIClient, discount_type: str,
     except HTTPError as err:
         logger.error(["[migrate_program_offers_to_commercetools] - Failed to get existing cart discount", err])
         return None
+
 
 def _get_highest_sort_order(client: CommercetoolsAPIClient):
     try:
@@ -59,6 +61,7 @@ def _get_highest_sort_order(client: CommercetoolsAPIClient):
 
     return 0.00000000000001
 
+
 def _update_cart_discount(client: CommercetoolsAPIClient, discount_id: str, predicate: str):
     try:
         response = client.update_cart_discount_target_predicate(discount_id, predicate)
@@ -69,10 +72,11 @@ def _update_cart_discount(client: CommercetoolsAPIClient, discount_id: str, pred
         )
         return None
 
+
 def _create_cart_discount(client: CommercetoolsAPIClient, discount_type: str, discount_value: int, sort_order: float,
-    predicate: str):
+                          predicate: str):
     try:
-        display_discount_value = int(discount_value/100)
+        display_discount_value = int(discount_value / 100)
         response = client.create_cart_discount_without_code(
             key=f"{discount_type}-{display_discount_value}-program-offer",
             name=f"{discount_type.capitalize()} {display_discount_value} Program Offer",
@@ -102,11 +106,13 @@ def _create_target_predicate_from_program_uuids(program_uuids: list, is_ten_perc
 
     return predicate
 
+
 # Function to extract UUIDs and condition type from a predicate
 def _combine_uuids_to_predicate(predicate: str, is_ten_percent_discount: bool, program_uuids: list):
     uuids = re.findall(r'custom\.bundleId\s*(?:!=|=)\s*"([^"]+)"', predicate)
     combined_uuids = list(set(uuids) | set(program_uuids))
     return _create_target_predicate_from_program_uuids(combined_uuids, is_ten_percent_discount)
+
 
 def _group_ten_percentage_offers(cart_discounts: list):
     offers = ConditionalOffer.objects.filter(
@@ -131,6 +137,7 @@ def _group_ten_percentage_offers(cart_discounts: list):
         "value": 10.00,
         "program_uuids": programs_to_exclude
     })
+
 
 def _group_other_offers(cart_discounts: list):
     """
@@ -195,9 +202,10 @@ class Command(BaseCommand):
                 discount_type == CT_PERCENTAGE_DISCOUNT_TYPE and ct_discount_value == TEN_PERCENT_DISCOUNT_PERMYRIAD
             )
 
-            if  existing['count'] == 0:
+            if existing['count'] == 0:
                 print('Creating new discount', discount_data)
                 sort_order += 0.00000000000001
+
                 _create_cart_discount(
                     client=client,
                     discount_type=discount_type,
